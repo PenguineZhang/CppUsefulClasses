35d9da4d80c4fc4909209dce5404bb1d365f43b8
peng <pengzhang40@gmail.com>
2022-05-06 11:38:31 -0700

    need polish for DisjointSet

--- DataStructures.cpp
+++ DataStructures.cpp
@@ -11,21 +11,45 @@ Trie::~Trie()
 
 }
 
-/*******************   Union Find   *******************/
-template <typename T>
-DisjointSet<T>::DisjointSet()
+/*******************   Union Find for std::pair data  *******************/
+DisjointSet::DisjointSet(std::vector<std::pair<int, int>>& data)
+{
+    for(std::pair<int, int>& d: data){
+        parents[d] = d;
+        rank[d] = 0;
+    }
+}
+
+DisjointSet::DisjointSet(std::unordered_map<std::pair<int, int>, std::pair<int, int>, hash_fn>& data)
+{
+    parents = data;
+}
+
+DisjointSet::~DisjointSet()
 {
 
 }
 
-template <typename T>
-DisjointSet<T>::~DisjointSet()
+std::pair<int, int> DisjointSet::Find(std::pair<int, int> x)
 {
+    if(x != parents[x]){
+        parents[x] = Find(parents[x]);
+    }
 
+    return parents[x];
 }
 
-template <typename T>
-T DisjointSet<T>::Find(T)
+void DisjointSet::Union(std::pair<int, int> x, std::pair<int, int> y)
 {
+    std::pair<int, int> px = Find(x);
+    std::pair<int, int> py = Find(y);
 
+    if(rank[px] > rank[py]){
+        parents[py] = px;
+    }else if(rank[px] < rank[py]){
+        parents[px] = py;
+    }else{
+        parents[py] = px;
+        rank[px]++;
+    }
 }
--- DataStructures.hpp
+++ DataStructures.hpp
@@ -1,4 +1,7 @@
 #include <vector>
+#include <unordered_map>
+#include <utility>
+
 
 class Trie
 {
@@ -7,21 +10,30 @@ class Trie
         ~Trie();
 };
 
-template <typename T>
 class DisjointSet
 {
-    public:
-        DisjointSet();
-        ~DisjointSet();
-        T Find(T);
-        void Union();
-
-        struct Node{
-            T val;
-            T* parent;
+    struct hash_fn
+        {   
+            template <typename T1, typename T2>
+            std::size_t operator() (const std::pair<T1, T2>& p) const
+            {
+                std::size_t h1 = std::hash<T1>()(p.first);
+                std::size_t h2 = std::hash<T2>()(p.second);
+                return h1 ^ h2;
+            }
         };
 
+    public:
+        DisjointSet() = delete;
+        DisjointSet(std::vector<std::pair<int, int>>& data);
+        DisjointSet(std::unordered_map<std::pair<int, int>, std::pair<int, int>, hash_fn>&);
+        ~DisjointSet();
+        std::pair<int, int> Find(std::pair<int, int>);
+        void Union(std::pair<int, int>, std::pair<int, int>);
+
+        
+
     private:
-        std::vector<int> ranks;
-        std::vector<T> parents;
+        std::unordered_map<std::pair<int, int>, int, hash_fn> rank;
+        std::unordered_map<std::pair<int, int>, std::pair<int, int>, hash_fn> parents;
 };
